6ff52131c8e4f2ab5e7b36b4edc1773d5274a077
airb <ayelen.i.rojas@gmail.com>
2021-12-16 22:54:44 +0100

    Changed CSV read function

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -1,10 +1,11 @@
 
 library(ggplot2)
 library(dplyr)
+library(data.table)
 
-ny = read.csv('new_york_city.csv', stringsAsFactors=FALSE)
-wash = read.csv('washington.csv', stringsAsFactors=FALSE)
-chi = read.csv('chicago.csv', stringsAsFactors=FALSE)
+ny = fread('new_york_city.csv', stringsAsFactors=FALSE)
+wash = fread('washington.csv', stringsAsFactors=FALSE)
+chi = fread('chicago.csv', stringsAsFactors=FALSE)
 
 head(ny)
 str(ny)
@@ -66,7 +67,7 @@ df$Start.Month = strftime(df$Start.Time.2, "%b", tz = "UTC")
 head(df$Start.Month)
 df$Start.Month = as.factor(df$Start.Month)
 levels(df$Start.Month)
-df$Start.Month = factor(df$Start.Month, levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun")) # 
+df$Start.Month = factor(df$Start.Month, levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun")) #
 # we change it to factor and reordered to plot it later on
 
 df$Start.Hour = strftime(df$Start.Time.2, "%H:%M:%S", tz = "UTC")
@@ -80,13 +81,13 @@ breaks = sort(c(unique(round(df$Birth.Year, digits = -1)), max(round(df$Birth.Ye
 # we create this vector so we can make breaks every 5 years.
 fd = subset(df, !(df$City == "Washington")) # subseted as it does not have Gender information
 
-ggplot(subset(fd, !is.na(fd$User.Type)), aes(x=Birth.Year, fill=User.Type)) + 
+ggplot(subset(fd, !is.na(fd$User.Type)), aes(x=Birth.Year, fill=User.Type)) +
 geom_histogram(binwidth = 1)+
     scale_x_continuous(breaks=seq(min(breaks), max(breaks), by = 5), limits = c(min(breaks), max(breaks)))+
-    labs(title = "Birth Year Distribution by User Type and City", y = "User count", x = "Birth Year") + 
+    labs(title = "Birth Year Distribution by User Type and City", y = "User count", x = "Birth Year") +
     theme(panel.background = element_blank(),
           panel.grid.major.y = element_line(size = 0.5, linetype = "dashed", colour = "darkgrey"),
-          axis.line.x = element_line(size = 0.5), 
+          axis.line.x = element_line(size = 0.5),
           axis.line.y = element_line(size = 0.5),
           axis.text.x = element_text(size=8,angle=45, hjust = 1),
           plot.title = element_text(size=16),
@@ -99,16 +100,16 @@ by(df$User.Type, df$City, table)
 summary(df$Birth.Year)
 
 summary(df[which(df$User.Type == "Subscriber" & df$City == "Chicago"), "Birth.Year"])
-summary(df[which(df$User.Type == "Subscriber" & df$City == "New York"), "Birth.Year"])       
+summary(df[which(df$User.Type == "Subscriber" & df$City == "New York"), "Birth.Year"])
 
 fd = subset(df, !(df$City == "Washington")) # subseted as it does not have Gender information
 fd = subset(fd, !is.na(fd$User.Type) & !is.na(fd$Gender)) # removing missings
-     
+
 ggplot(fd, aes(x = Gender, fill = Gender)) +
-geom_bar() + 
+geom_bar() +
     scale_fill_manual(values = c("Female" = "#5D3A9B", "Male" = "#E66100")) +
-    labs(title = "Gender Distribution by User Type and City", y = "User count", x = element_blank()) + 
-    theme(legend.position = "none", 
+    labs(title = "Gender Distribution by User Type and City", y = "User count", x = element_blank()) +
+    theme(legend.position = "none",
           panel.background = element_blank(),
           panel.grid.major.y = element_line(size = 0.5, linetype = "dashed", colour = "darkgrey"),
           strip.background = element_rect(fill = "#ededed", colour = "#ededed", size = 1),
@@ -117,20 +118,20 @@ geom_bar() +
           axis.text.y = element_text(size=9),
           plot.title = element_text(size=16)) +
 
-    facet_wrap(~User.Type + City) 
+    facet_wrap(~User.Type + City)
 
 by(fd$Gender, list(fd$User.Type, fd$City), table)
 
-fd = subset(df, !(df$City == "Washington") & !is.na(df$User.Type) & !is.na(df$Gender)) 
+fd = subset(df, !(df$City == "Washington") & !is.na(df$User.Type) & !is.na(df$Gender))
 # removing missings
 
-ggplot(subset(fd, !is.na(fd$User.Type)), aes(x=Birth.Year, fill=User.Type)) + 
+ggplot(subset(fd, !is.na(fd$User.Type)), aes(x=Birth.Year, fill=User.Type)) +
 geom_histogram(binwidth = 1)+
   scale_x_continuous(breaks=seq(min(breaks), max(breaks), by = 10), limits = c(min(breaks), max(breaks)))+
-  labs(title = "Birth Year Distribution by User Type, City and Sex", y = "User count", x = "Birth Year") + 
+  labs(title = "Birth Year Distribution by User Type, City and Sex", y = "User count", x = "Birth Year") +
   theme(panel.background = element_blank(),
         panel.grid.major.y = element_line(size = 0.5, linetype = "dashed", colour = "darkgrey"),
-        axis.line.x = element_line(size = 0.5), 
+        axis.line.x = element_line(size = 0.5),
         axis.line.y = element_line(size = 0.5),
         axis.text.x = element_text(size=8, angle=45, hjust = 1),
         plot.title = element_text(size=16),
@@ -142,13 +143,13 @@ fd = df
 fd$User.Type = "Both" # to compare everything
 fd = rbind(df, fd)
 
-ggplot(subset(fd, !is.na(User.Type)), aes(x = City, y = Trip.Duration.min, fill = City)) + 
+ggplot(subset(fd, !is.na(User.Type)), aes(x = City, y = Trip.Duration.min, fill = City)) +
 geom_boxplot(outlier.colour = "#a9a9a9") +
-  labs(title = "Trip Duration Distribution by User Type and City", y = "Trip Duration (min.)", x = element_blank()) + 
+  labs(title = "Trip Duration Distribution by User Type and City", y = "Trip Duration (min.)", x = element_blank()) +
   scale_fill_manual(values = c("Chicago" = "#70D357", "New York" = "#DFC721", "Washington" = "#3EB9CE")) +
   coord_cartesian(ylim = c(0, 120)) + # to omit outliers
-  facet_wrap(~User.Type) + 
-  theme(legend.position = "none", 
+  facet_wrap(~User.Type) +
+  theme(legend.position = "none",
         panel.background = element_blank(),
         panel.grid.major.y = element_line(size = 0.5, linetype = "dashed", colour = "darkgrey"),
         strip.background = element_rect(fill = "#ededed", colour = "#ededed", size = 1),
@@ -163,21 +164,21 @@ by(df$"Trip.Duration.min", list(df$City, df$User.Type), summary)
 
 fd = subset(df, !is.na(df$User.Type)) # removing missings
 
-ggplot(fd, aes(x=Start.Hour.2, fill = User.Type)) + 
+ggplot(fd, aes(x=Start.Hour.2, fill = User.Type)) +
 geom_histogram(stat = "count") +
   scale_x_datetime(date_labels = "%Hh",breaks = "2 hours") +
-  labs(title = "Start Time Distribution by User Type and City", 
-       x = "Hours", y = "Start Time") + 
+  labs(title = "Start Time Distribution by User Type and City",
+       x = "Hours", y = "Start Time") +
   theme(panel.background = element_blank(),
         panel.grid.major.y = element_line(size = 0.1, linetype = "dashed", colour = "darkgrey"),
         panel.grid.minor.x = element_line(size = 0.1, linetype = "dashed", colour = "darkgrey"),
-        axis.line.x = element_line(size = 0.5), 
+        axis.line.x = element_line(size = 0.5),
         axis.line.y = element_line(size = 0.5),
         axis.text.x = element_text(size=8, angle=45, hjust = 1),
         plot.title = element_text(size=16),
        legend.position = "none") +
   scale_fill_manual(values = c("Customer" = "#f1b9a5", "Subscriber" = "#83c5be")) +
-  
+
 facet_wrap(~User.Type + City, scales = "free", ncol = 3)
 
 fd$Start.Hour.3 = format(strptime(fd$Start.Hour.2,"%Y-%m-%d %H:%M:%S"),'%H')
